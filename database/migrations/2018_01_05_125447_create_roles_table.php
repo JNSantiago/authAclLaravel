8490dc581e8c519a6b9e30ee02e4ca63aef042ba
joao <joao.neto.ninja@hotmail.com>
2018-01-05 09:56:16 -0300

    updating migration for role table

--- database/migrations/2018_01_05_125447_create_roles_table.php
+++ database/migrations/2018_01_05_125447_create_roles_table.php
@@ -15,6 +15,9 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->jsonb('permissions')->default('{}');
             $table->timestamps();
         });
     }
